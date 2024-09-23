81cc52527d9d1738196713af2aded5d3fb821de7
lukasschlegel <150013981+lukasschlegel@users.noreply.github.com>
2024-09-23 21:52:51 +0200

    globe night

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function getEnergyData() {
     }
 
     // Beispiel-URL, an die Anfrage gesendet wird. Die API-URL muss angepasst werden, um das Land abzufragen.
-    const apiUrl = `https://api.energy-charts.info/query?country=${encodeURIComponent(country)}`;
+    const apiUrl = `https://api.energy-charts.info/power/public_power/?country=DE`;
 
     try {
         const response = await fetch(apiUrl);
@@ -25,3 +25,25 @@ async function getEnergyData() {
         resultDiv.innerHTML = "Fehler beim Abrufen der Daten: " + error.message;
     }
 }
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibHVrYXNzY2hsZWdlbCIsImEiOiJjbHc2Y2drMGIxcGhjMnFwaG1wc21mZ3U4In0.BRdsjYzHNpwgSXhnbXVvzA';
+const map = new mapboxgl.Map({
+container: 'map',
+// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+style: 'mapbox://styles/mapbox/dark-v11',
+zoom: 3.1,
+center: [15, 50],
+projection: 'globe'
+});
+ 
+map.on('load', () => {
+// Set the default atmosphere style
+map.setFog({
+    "range": [0.8, 8],
+    "color": "black",
+    "horizon-blend": 0.01,
+    "high-color": "lightblue",
+    "space-color": "#000000",
+    "star-intensity": 0.03
+});
+});
