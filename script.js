84a25e7651f096038ce1777758f8ac401b0e27d3
lukasschlegel <150013981+lukasschlegel@users.noreply.github.com>
2024-09-29 14:48:03 +0200

    countryData aktualisiert und marker in die Hauptstädte verlegt

--- script.js
+++ script.js
@@ -89,99 +89,51 @@ function renderChart() {
 }
 
 const countryData = {
-    'DE': { name: 'Germany', coordinates: [10.4515, 51.1657], aliases: ['Deutschland', 'DE'] },
-    'FR': { name: 'France', coordinates: [2.2137, 46.2276], aliases: ['Frankreich', 'FR'] },
-    'IT': { name: 'Italy', coordinates: [12.5674, 41.8719], aliases: ['Italien', 'IT'] },
-    'ES': { name: 'Spain', coordinates: [-3.7492, 40.4637], aliases: ['Spanien', 'ES'] },
-    'AT': { name: 'Austria', coordinates: [13.3333, 47.5162], aliases: ['Österreich', 'AT'] },
-    'GB': { name: 'United Kingdom', coordinates: [-1.1743, 52.3555], aliases: ['England', 'GB', 'UK', 'Vereinigtes Königreich'] },
-    'SE': { name: 'Sweden', coordinates: [18.6435, 60.1282], aliases: ['Schweden', 'SE'] },
-    'DK': { name: 'Denmark', coordinates: [9.5018, 56.2639], aliases: ['Dänemark', 'DK'] },
-    'CH': { name: 'Switzerland', coordinates: [8.2275, 46.8182], aliases: ['Schweiz', 'CH'] },
-    'PT': { name: 'Portugal', coordinates: [-8.2245, 39.3999], aliases: ['Portugal', 'PT'] },
-    'NO': { name: 'Norway', coordinates: [8.4689, 60.472], aliases: ['Norwegen', 'NO'] },
-    'FI': { name: 'Finland', coordinates: [25.7482, 61.9241], aliases: ['Finnland', 'FI'] },
-    'PL': { name: 'Poland', coordinates: [19.1451, 51.9194], aliases: ['Polen', 'PL'] },
-    'CZ': { name: 'Czech Republic', coordinates: [15.4729, 49.8175], aliases: ['Tschechien', 'CZ', 'Czechia'] },
-    'HU': { name: 'Hungary', coordinates: [19.5033, 47.1625], aliases: ['Ungarn', 'HU'] },
-    'GR': { name: 'Greece', coordinates: [21.8243, 39.0742], aliases: ['Griechenland', 'GR'] },
-    'NL': { name: 'Netherlands', coordinates: [5.2913, 52.1326], aliases: ['Niederlande', 'NL', 'Holland'] },
-    'BE': { name: 'Belgium', coordinates: [4.4699, 50.5039], aliases: ['Belgien', 'BE'] },
-    'RO': { name: 'Romania', coordinates: [24.9668, 45.9432], aliases: ['Rumänien', 'RO'] },
-    'BG': { name: 'Bulgaria', coordinates: [25.4858, 42.7339], aliases: ['Bulgarien', 'BG'] },
-    'HR': { name: 'Croatia', coordinates: [15.2, 45.1], aliases: ['Kroatien', 'HR'] },
-    'RS': { name: 'Serbia', coordinates: [21.0059, 44.0165], aliases: ['Serbien', 'RS'] },
-    'SI': { name: 'Slovenia', coordinates: [14.9955, 46.1512], aliases: ['Slowenien', 'SI'] },
-    'SK': { name: 'Slovakia', coordinates: [19.699, 48.669], aliases: ['Slowakei', 'SK'] },
-    'EE': { name: 'Estonia', coordinates: [25.0136, 58.5953], aliases: ['Estland', 'EE'] },
-    'LV': { name: 'Latvia', coordinates: [24.6032, 56.8796], aliases: ['Lettland', 'LV'] },
-    'LT': { name: 'Lithuania', coordinates: [23.8813, 55.1694], aliases: ['Litauen', 'LT'] },
-    'IE': { name: 'Ireland', coordinates: [-8.2439, 53.4129], aliases: ['Irland', 'IE'] },
-    'IS': { name: 'Iceland', coordinates: [-19.0208, 64.9631], aliases: ['Island', 'IS'] },
-    'LU': { name: 'Luxembourg', coordinates: [6.1296, 49.6117], aliases: ['Luxemburg', 'LU'] },
-    'MT': { name: 'Malta', coordinates: [14.3754, 35.9375], aliases: ['MT'] },
-    'CY': { name: 'Cyprus', coordinates: [33.4299, 35.1264], aliases: ['Zypern', 'CY'] },
-    'AL': { name: 'Albania', coordinates: [20.1683, 41.1533], aliases: ['Albanien', 'AL'] },
-    'MK': { name: 'North Macedonia', coordinates: [21.7453, 41.6086], aliases: ['Nordmazedonien', 'MK'] },
-    'MD': { name: 'Moldova', coordinates: [28.3699, 47.4116], aliases: ['Moldawien', 'MD'] },
-    'UA': { name: 'Ukraine', coordinates: [31.1656, 48.3794], aliases: ['Ukraine', 'UA'] },
-    'BY': { name: 'Belarus', coordinates: [27.9534, 53.7098], aliases: ['Weißrussland', 'BY'] },
-    'BA': { name: 'Bosnia and Herzegovina', coordinates: [17.6791, 43.9159], aliases: ['Bosnien und Herzegowina', 'BA'] },
-    'ME': { name: 'Montenegro', coordinates: [19.3744, 42.7087], aliases: ['Montenegro', 'ME'] },
-    'XK': { name: 'Kosovo', coordinates: [20.902, 42.6026], aliases: ['Kosovo', 'XK'] },
-    'LI': { name: 'Liechtenstein', coordinates: [9.5554, 47.166], aliases: ['Liechtenstein', 'LI'] },
-    'VA': { name: 'Vatican City', coordinates: [12.4534, 41.9029], aliases: ['Vatikanstadt', 'VA'] },
-    'SM': { name: 'San Marino', coordinates: [12.4578, 43.9424], aliases: ['San Marino', 'SM'] },
-    'AD': { name: 'Andorra', coordinates: [1.5211, 42.5063], aliases: ['Andorra', 'AD'] },
-    'MC': { name: 'Monaco', coordinates: [7.4128, 43.7384], aliases: ['Monaco', 'MC'] }
-};
-
-const capitalTranslations = {
-    'Germany': 'Berlin',
-    'France': 'Paris',
-    'Italy': 'Rom',
-    'Spain': 'Madrid',
-    'Austria': 'Wien',
-    'United Kingdom': 'London',
-    'Sweden': 'Stockholm',
-    'Denmark': 'Kopenhagen',
-    'Switzerland': 'Bern',
-    'Portugal': 'Lissabon',
-    'Norway': 'Oslo',
-    'Finland': 'Helsinki',
-    'Poland': 'Warschau',
-    'Czech Republic': 'Prag',
-    'Hungary': 'Budapest',
-    'Greece': 'Athen',
-    'Netherlands': 'Amsterdam',
-    'Belgium': 'Brüssel',
-    'Romania': 'Bukarest',
-    'Bulgaria': 'Sofia',
-    'Croatia': 'Zagreb',
-    'Serbia': 'Belgrad',
-    'Slovenia': 'Ljubljana',
-    'Slovakia': 'Bratislava',
-    'Estonia': 'Tallinn',
-    'Latvia': 'Riga',
-    'Lithuania': 'Vilnius',
-    'Ireland': 'Dublin',
-    'Iceland': 'Reykjavik',
-    'Luxembourg': 'Luxemburg',
-    'Malta': 'Valletta',
-    'Cyprus': 'Nikosia',
-    'Albania': 'Tirana',
-    'North Macedonia': 'Skopje',
-    'Moldova': 'Chisinau',
-    'Ukraine': 'Kiew',
-    'Belarus': 'Minsk',
-    'Bosnia and Herzegovina': 'Sarajevo',
-    'Montenegro': 'Podgorica',
-    'Kosovo': 'Pristina',
-    'Liechtenstein': 'Vaduz',
-    'Vatican City': 'Vatikanstadt',
-    'San Marino': 'San Marino',
-    'Andorra': 'Andorra la Vella',
-    'Monaco': 'Monaco'
+    'DE': { name: 'Germany', coordinates: [10.4515, 51.1657], capital: [13.4050, 52.5200], capitalTranslation: 'Berlin', aliases: ['Deutschland', 'DE'] },
+    'FR': { name: 'France', coordinates: [2.2137, 46.2276], capital: [2.3522, 48.8566], capitalTranslation: 'Paris', aliases: ['Frankreich', 'FR'] },
+    'IT': { name: 'Italy', coordinates: [12.5674, 41.8719], capital: [12.4964, 41.9028], capitalTranslation: 'Rom', aliases: ['Italien', 'IT'] },
+    'ES': { name: 'Spain', coordinates: [-3.7492, 40.4637], capital: [-3.7038, 40.4168], capitalTranslation: 'Madrid', aliases: ['Spanien', 'ES'] },
+    'AT': { name: 'Austria', coordinates: [13.3333, 47.5162], capital: [16.3738, 48.2082], capitalTranslation: 'Wien', aliases: ['Österreich', 'AT'] },
+    'GB': { name: 'United Kingdom', coordinates: [-1.1743, 52.3555], capital: [-0.1276, 51.5074], capitalTranslation: 'London', aliases: ['England', 'GB', 'UK', 'Vereinigtes Königreich'] },
+    'SE': { name: 'Sweden', coordinates: [18.6435, 60.1282], capital: [18.0686, 59.3293], capitalTranslation: 'Stockholm', aliases: ['Schweden', 'SE'] },
+    'DK': { name: 'Denmark', coordinates: [9.5018, 56.2639], capital: [12.5683, 55.6761], capitalTranslation: 'Kopenhagen', aliases: ['Dänemark', 'DK'] },
+    'CH': { name: 'Switzerland', coordinates: [8.2275, 46.8182], capital: [7.4474, 46.9481], capitalTranslation: 'Bern', aliases: ['Schweiz', 'CH'] },
+    'PT': { name: 'Portugal', coordinates: [-8.2245, 39.3999], capital: [-9.1393, 38.7223], capitalTranslation: 'Lissabon', aliases: ['Portugal', 'PT'] },
+    'NO': { name: 'Norway', coordinates: [8.4689, 60.472], capital: [10.7522, 59.9139], capitalTranslation: 'Oslo', aliases: ['Norwegen', 'NO'] },
+    'FI': { name: 'Finland', coordinates: [25.7482, 61.9241], capital: [24.9384, 60.1695], capitalTranslation: 'Helsinki', aliases: ['Finnland', 'FI'] },
+    'PL': { name: 'Poland', coordinates: [19.1451, 51.9194], capital: [21.0122, 52.2297], capitalTranslation: 'Warschau', aliases: ['Polen', 'PL'] },
+    'CZ': { name: 'Czech Republic', coordinates: [15.4729, 49.8175], capital: [14.4378, 50.0755], capitalTranslation: 'Prag', aliases: ['Tschechien', 'CZ', 'Czechia'] },
+    'HU': { name: 'Hungary', coordinates: [19.5033, 47.1625], capital: [19.0402, 47.4979], capitalTranslation: 'Budapest', aliases: ['Ungarn', 'HU'] },
+    'GR': { name: 'Greece', coordinates: [21.8243, 39.0742], capital: [23.7275, 37.9838], capitalTranslation: 'Athen', aliases: ['Griechenland', 'GR'] },
+    'NL': { name: 'Netherlands', coordinates: [5.2913, 52.1326], capital: [4.9041, 52.3676], capitalTranslation: 'Amsterdam', aliases: ['Niederlande', 'NL', 'Holland'] },
+    'BE': { name: 'Belgium', coordinates: [4.4699, 50.5039], capital: [4.3517, 50.8503], capitalTranslation: 'Brüssel', aliases: ['Belgien', 'BE'] },
+    'RO': { name: 'Romania', coordinates: [24.9668, 45.9432], capital: [26.1025, 44.4268], capitalTranslation: 'Bukarest', aliases: ['Rumänien', 'RO'] },
+    'BG': { name: 'Bulgaria', coordinates: [25.4858, 42.7339], capital: [23.3219, 42.6977], capitalTranslation: 'Sofia', aliases: ['Bulgarien', 'BG'] },
+    'HR': { name: 'Croatia', coordinates: [15.2, 45.1], capital: [15.9819, 45.8150], capitalTranslation: 'Zagreb', aliases: ['Kroatien', 'HR'] },
+    'RS': { name: 'Serbia', coordinates: [21.0059, 44.0165], capital: [20.4573, 44.7866], capitalTranslation: 'Belgrad', aliases: ['Serbien', 'RS'] },
+    'SI': { name: 'Slovenia', coordinates: [14.9955, 46.1512], capital: [14.5058, 46.0569], capitalTranslation: 'Ljubljana', aliases: ['Slowenien', 'SI'] },
+    'SK': { name: 'Slovakia', coordinates: [19.699, 48.669], capital: [17.1077, 48.1486], capitalTranslation: 'Bratislava', aliases: ['Slowakei', 'SK'] },
+    'EE': { name: 'Estonia', coordinates: [25.0136, 58.5953], capital: [24.7536, 59.4370], capitalTranslation: 'Tallinn', aliases: ['Estland', 'EE'] },
+    'LV': { name: 'Latvia', coordinates: [24.6032, 56.8796], capital: [24.1058, 56.9496], capitalTranslation: 'Riga', aliases: ['Lettland', 'LV'] },
+    'LT': { name: 'Lithuania', coordinates: [23.8813, 55.1694], capital: [25.2799, 54.6872], capitalTranslation: 'Vilnius', aliases: ['Litauen', 'LT'] },
+    'IE': { name: 'Ireland', coordinates: [-8.2439, 53.4129], capital: [-6.2603, 53.3498], capitalTranslation: 'Dublin', aliases: ['Irland', 'IE'] },
+    'IS': { name: 'Iceland', coordinates: [-19.0208, 64.9631], capital: [-21.8174, 64.1265], capitalTranslation: 'Reykjavik', aliases: ['Island', 'IS'] },
+    'LU': { name: 'Luxembourg', coordinates: [6.1296, 49.6117], capital: [6.1319, 49.6117], capitalTranslation: 'Luxemburg', aliases: ['Luxemburg', 'LU'] },
+    'MT': { name: 'Malta', coordinates: [14.3754, 35.9375], capital: [14.5146, 35.8997], capitalTranslation: 'Valletta', aliases: ['MT'] },
+    'CY': { name: 'Cyprus', coordinates: [33.4299, 35.1264], capital: [33.3823, 35.1856], capitalTranslation: 'Nikosia', aliases: ['Zypern', 'CY'] },
+    'AL': { name: 'Albania', coordinates: [20.1683, 41.1533], capital: [19.8189, 41.3275], capitalTranslation: 'Tirana', aliases: ['Albanien', 'AL'] },
+    'MK': { name: 'North Macedonia', coordinates: [21.7453, 41.6086], capital: [21.4316, 41.9981], capitalTranslation: 'Skopje', aliases: ['Nordmazedonien', 'MK'] },
+    'MD': { name: 'Moldova', coordinates: [28.3699, 47.4116], capital: [28.8638, 47.0105], capitalTranslation: 'Chisinau', aliases: ['Moldawien', 'MD'] },
+    'UA': { name: 'Ukraine', coordinates: [31.1656, 48.3794], capital: [30.5234, 50.4501], capitalTranslation: 'Kiew', aliases: ['Ukraine', 'UA'] },
+    'BY': { name: 'Belarus', coordinates: [27.9534, 53.7098], capital: [27.5666, 53.9006], capitalTranslation: 'Minsk', aliases: ['Weißrussland', 'BY'] },
+    'BA': { name: 'Bosnia and Herzegovina', coordinates: [17.6791, 43.9159], capital: [18.4131, 43.8563], capitalTranslation: 'Sarajevo', aliases: ['Bosnien und Herzegowina', 'BA'] },
+    'ME': { name: 'Montenegro', coordinates: [19.3744, 42.7087], capital: [19.2594, 42.4304], capitalTranslation: 'Podgorica', aliases: ['Montenegro', 'ME'] },
+    'XK': { name: 'Kosovo', coordinates: [20.902, 42.6026], capital: [21.1655, 42.6629], capitalTranslation: 'Pristina', aliases: ['Kosovo', 'XK'] },
+    'LI': { name: 'Liechtenstein', coordinates: [9.5554, 47.166], capital: [9.5215, 47.1416], capitalTranslation: 'Vaduz', aliases: ['Liechtenstein', 'LI'] },
+    'VA': { name: 'Vatican City', coordinates: [12.4534, 41.9029], capital: [12.4534, 41.9029], capitalTranslation: 'Vatikanstadt', aliases: ['Vatikanstadt', 'VA'] },
+    'SM': { name: 'San Marino', coordinates: [12.4578, 43.9424], capital: [12.4418, 43.9333], capitalTranslation: 'San Marino', aliases: ['San Marino', 'SM'] },
+    'AD': { name: 'Andorra', coordinates: [1.5211, 42.5063], capital: [1.5341, 42.5078], capitalTranslation: 'Andorra la Vella', aliases: ['Andorra', 'AD'] },
+    'MC': { name: 'Monaco', coordinates: [7.4128, 43.7384], capital: [7.4190, 43.7311], capitalTranslation: 'Monaco', aliases: ['Monaco', 'MC'] }
 };
 
 function checkEnter(event) {
@@ -195,7 +147,6 @@ let currentMarker = null;  // Store the current marker
 
 async function getEnergyData() {
     const countryInput = document.getElementById('countryInput').value.trim().toLowerCase(); 
-    const resultDiv = document.getElementById('result');
     let countryCode = null;
 
     // Clear previous marker, chart visibility, flag image, and capital text
@@ -224,21 +175,21 @@ async function getEnergyData() {
     }
 
     if (!countryCode) {
-        resultDiv.innerHTML = "Land nicht gefunden.";
         document.getElementById('loadingPlaceholder').style.display = 'none'; // Hide loading if country not found
+        alert("Land nicht gefunden.");
         return;
     }
 
-    // Fly to the selected country
-    const coordinates = countryData[countryCode].coordinates;
+    // Fly to the capital of the selected country
+    const capitalCoordinates = countryData[countryCode].capital;
     map.flyTo({
-        center: coordinates,
-        zoom: 5,
+        center: capitalCoordinates,
+        zoom: 5.5,
         essential: true
     });
 
-    // Add a marker to the center of the country
-    addCountryMarker(coordinates);
+    // Add a marker to the capital city of the country
+    addCountryMarker(capitalCoordinates);
 
     // Display the chart only if Switzerland is selected
     if (countryCode === 'CH') {
@@ -248,7 +199,7 @@ async function getEnergyData() {
     // Fetch the population and flag data
     try {
         const populationApiUrl = 'https://countriesnow.space/api/v0.1/countries/population';
-        const populationResponse = fetch(populationApiUrl, {
+        const populationResponse = await fetch(populationApiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -259,7 +210,7 @@ async function getEnergyData() {
         });
 
         const flagApiUrl = 'https://countriesnow.space/api/v0.1/countries/flag/images';
-        const flagResponse = fetch(flagApiUrl, {
+        const flagResponse = await fetch(flagApiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -275,19 +226,19 @@ async function getEnergyData() {
         const flagJson = await flagData.json();
 
         if (populationJson.error || !populationJson.data || flagJson.error || !flagJson.data) {
-            resultDiv.innerHTML = "Daten konnten nicht gefunden werden.";
+            alert("Daten konnten nicht gefunden werden.");
             document.getElementById('loadingPlaceholder').style.display = 'none'; // Hide loading if there's an error
             return;
         }
 
+        // Set the population and capital text
         const population = populationJson.data.populationCounts[populationJson.data.populationCounts.length - 1].value;
         document.getElementById('populationText').innerText = `${population.toLocaleString()} Einwohner`;
         
-        // Use the translation table to get the capital in German
-        const countryName = countryData[countryCode].name;
-        const capital = capitalTranslations[countryName] || 'Unbekannt'; // Fallback to 'Unbekannt' if not in the table
-        document.getElementById('capitalText').innerText = `Hauptstadt: ${capital}`; // Set the capital text
-        
+        // Use the translation from the countryData to get the capital in German
+        const capitalTranslation = countryData[countryCode].capitalTranslation || 'Unbekannt';
+        document.getElementById('capitalText').innerText = `Hauptstadt: ${capitalTranslation}`;
+
         // Set the flag image and show it
         document.getElementById('flagImage').src = flagJson.data.flag;
         document.getElementById('flagImage').style.display = 'block'; // Show flag once it's loaded
@@ -299,7 +250,7 @@ async function getEnergyData() {
         document.getElementById('capitalText').style.display = 'block'; // Show the capital
 
     } catch (error) {
-        resultDiv.innerHTML = "Fehler beim Abrufen der Daten: " + error.message;
+        alert("Fehler beim Abrufen der Daten: " + error.message);
         document.getElementById('loadingPlaceholder').style.display = 'none'; // Hide loading on error
     }
 }
