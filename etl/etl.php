bc41478604961bd2df1295aced4191b750003785
lukasschlegel <150013981+lukasschlegel@users.noreply.github.com>
2024-10-01 14:19:57 +0200

    etl.php update alle länder

--- etl/etl.php
+++ etl/etl.php
@@ -2,16 +2,22 @@
 
 // Funktion zum Abrufen der Daten für ein bestimmtes Land
 function fetchPowerData($countryCode) {
-    $url = "https://api.energy-charts.info/public_power?country=" . $countryCode;
+    $url = "https://api.energy-charts.info/public_power?country=$countryCode";
 
     // Initialisiert eine cURL-Sitzung
     $ch = curl_init($url);
 
     // Setzt Optionen
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FAILONERROR, true); // cURL gibt Fehler zurück, wenn etwas schief geht
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
     $response = curl_exec($ch);
+
+    // Fehlerüberprüfung für cURL
+    if (curl_errno($ch)) {
+        echo 'cURL-Fehler: ' . curl_error($ch);
+    }
 
     // Schließt die cURL-Sitzung
     curl_close($ch);
@@ -20,21 +26,23 @@ function fetchPowerData($countryCode) {
     return json_decode($response, true);
 }
 
-// Liste der Länder-Codes (Beispiele)
-$countries = ['ch', 'de', 'fr', 'at', 'it']; // Hier kannst du die verfügbaren Länder-Codes ergänzen
+// Liste der Länder
+$countries = ['ch', 'eu', 'all', 'al', 'am', 'at', 'az', 'ba', 'be', 'bg', 'by', 'cy', 'cz', 'dk', 'ee', 'es', 'fi', 'fr', 'ge', 'gr', 'hr', 'hu', 'ie', 'it', 'lt', 'lu', 'lv', 'md', 'me', 'mk', 'mt', 'nie', 'nl', 'no', 'pl', 'pt', 'ro', 'rs', 'ru', 'se', 'sl', 'sk', 'tr', 'ua', 'uk', 'xk'];
 
-// Schleife über alle Länder und Daten abrufen
+
+// Schleife über alle Länder
 foreach ($countries as $countryCode) {
     echo "Daten für Land: $countryCode\n";
+
     $data = fetchPowerData($countryCode);
 
     // Überprüfen, ob Daten abgerufen wurden
     if ($data) {
-        // Daten anzeigen oder weiterverarbeiten
-        var_dump($data);
+        var_dump($data['production_types']);
     } else {
-        echo "Fehler beim Abrufen der Daten für $countryCode\n";
+        echo "Keine Daten für $countryCode erhalten oder Fehler bei der API-Abfrage.\n";
     }
 }
 
 ?>
+
